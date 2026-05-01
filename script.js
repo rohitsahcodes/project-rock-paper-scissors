b969a783f2602adc8c98faee0ed42f292adf4fe7
rohitsahcodes <rohitsah.codes@gmail.com>
2026-05-01 21:07:02 +0545

    Functionality added using DOM

--- script.js
+++ script.js
@@ -27,41 +27,41 @@ const scissorsChoosen = document.querySelector(".btn.scissors");
 const comScrBtn = document.querySelector("#comScrBtn");
 const humScrBtn = document.querySelector("#humScrBtn");
 
-//receiving usier chice using click listener.. 
-rockChoosen.addEventListener("click", () => {
+//receiving user choice using click listener.. 
+rockChoosen.addEventListener("click", (e) => {
     humanChoice = "rock";
     getWinLost(humanChoice, getComputerChoice());
-    console.log("Human Score: ",humanScore);
-    console.log("Computer Score: ", computerScore);
     comScrBtn.textContent = computerScore;
     humScrBtn.textContent = humanScore;
-
 });
 
-
-
-
-
-paperChoosen.addEventListener("click", () => {
+paperChoosen.addEventListener("click", (e) => {
     humanChoice = "paper";
     // console.log(humanChoice);
     getWinLost(humanChoice, getComputerChoice());
+    comScrBtn.textContent = computerScore;
+    humScrBtn.textContent = humanScore;
 });
-scissorsChoosen.addEventListener("click", () => {
+scissorsChoosen.addEventListener("click", (e) => {
     humanChoice = "scissors";
     // console.log(humanChoice);
     getWinLost(humanChoice, getComputerChoice());
+    comScrBtn.textContent = computerScore;
+    humScrBtn.textContent = humanScore;
 });
 
 let humanScore = 0;
 let computerScore = 0;
-console.log(humanScore);
-console.log(computerScore);
 
-
+comScrBtn.textContent = computerScore;
+humScrBtn.textContent = humanScore;
 
 
 // taking computer and human choice and checkin first which case satisfy and then resulting the winner accordingly
+const mym = document.querySelector("#mym");
+const mymInfo = document.querySelector("#mymInfo");
+// console.log(mymInfo);
+const infoAboutGame = document.querySelector("#infoAboutGame");
 
 
 function getWinLost(hum, com) {
@@ -70,56 +70,94 @@ function getWinLost(hum, com) {
     if ((hum === "rock" || com === "rock") && (hum === "paper" || com === "paper")) {
         if (hum === "rock") {
             console.log("You lost!");
+            mym.textContent = "You lose!";
+            mymInfo.textContent = "(paper beats rock)";
+
             computerScore++;
         } else {
             console.log("You won!");
+            mym.textContent = "You win!";
+            mymInfo.textContent = "(paper beats rock)";
             humanScore++;
         }
     } else if ((hum === "rock" || com === "rock") && (hum === "scissors" || com === "scissors")) {
         if (hum === "scissors") {
-            console.log("You lost!");
+            console.log("You lose!");
+            mym.textContent = "You lose!";
+            mymInfo.textContent = "(rock beats scissors)";
             computerScore++;
         } else {
             console.log("You won!");
+            mym.textContent = "You win!";
+            mymInfo.textContent = "(rock beats scissors)";
             humanScore++;
         }
     } else if ((hum === "paper" || com === "paper") && (hum === "scissors" || com === "scissors")) {
         if (hum === "paper") {
-            console.log("You lost!");
+            console.log("You lose!");
+            mym.textContent = "You lose!";
+            mymInfo.textContent = "(scissors beats paper)";
             computerScore++;
         } else {
             console.log("You won!");
+            mym.textContent = "You win!";
+            mymInfo.textContent = "(scissors beats paper)";
             humanScore++;
         }
     }
     else {
-        console.log("It's is tie, babay");
+        console.log("It's tie, baby!");
+        mym.textContent = "It's a tie, baby!";
+        mymInfo.textContent = `Both choosed "${humanChoice}"`;
     }
+
+    if (humanScore === 3 || computerScore === 3) {
+        playAgain();
+    }
+
+}
+
+const playAgainShow = document.querySelector("#result");
+
+function playAgain() {
+    const playAgainDiv = document.createElement("div");
+    playAgainShow.innerHTML = "";
+    playAgainDiv.textContent = "Play AGAIN!";
+
+    playAgainDiv.id = "mym";
+
+    playAgainShow.appendChild(playAgainDiv);
+
+    newGame();
+
 }
 
 
-getWinLost(humanChoice, getComputerChoice());
+function newGame() {
+    if (humanScore === 3) {
+        // mym.textContent= "Wooh!";
+        // mymInfo.textContent = "You defeated Computer!"; 
+        infoAboutGame.textContent = "Wohh! You defeated computer!";
+    } else if (computerScore === 3) {
+        // mym.textContent = "Computer defeated you!";
+        // mymInfo.textContent = "Better luck next time"
+        infoAboutGame.textContent = "Computer deafeated you! Better luck next time";
+    } else {
+        infoAboutGame.textContent = "Those who score 3 first gonna win.. Have Fun!";
+    }
+
+
+
+    humanScore = 0;
+    computerScore = 0;
+    mym.textContent = "Make your move!";
+    mymInfo.textContent = null;
+}
+
 
 /*
 
 
-
-for (let i = 1; i <= 5; i++) {
-
-
-    getComputerChoice();
-    console.log("computer choice : ", getComputerChoice());
-
-    let temp = getHumanChoice();
-    console.log("human choice : ", temp);
-    getWinLost(humanChoice, computerChoice);
-
-    console.log("HumanScore: ", humanScore);
-    console.log("ComputerScore: ", computerScore);
-
-    console.log(i, " round completed");
-
-}
 
 if (humanScore > computerScore) {
     prompt("You deafeated computer!");
